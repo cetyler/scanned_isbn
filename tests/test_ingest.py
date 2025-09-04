5e734beafe78e3f083f062d626a785ab4cb8b059
Christopher Tyler <christophertyler@engineer.com>
2025-09-03 20:32:45 -0500

    WIP: Tests now pass, though will need to put the test data in the commit.

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -8,15 +8,18 @@ import datetime
 def convert(date_time):
     format = '%Y-%m-%dT%H:%M:%S%z'
     datetime_str = datetime.datetime.strptime(date_time, format)
-
+    
     return datetime_str.replace(tzinfo=None) + datetime.timedelta(hours=5) # remove timezone and add offset
 
-def test_load():
-    test_file = Path.cwd() / 'data' / 'scanned_isbn.csv'
+def test_load(tmp_path):
+    scanned_isbn_file = Path.cwd() / 'data' / 'scanned_isbn.csv'
+    authors_file = Path.cwd() / 'data' / 'authors.csv'
+    output_file = tmp_path / 'output.duckdb'
+    editions_file = Path.cwd() / 'data' / 'editions.csv'
 
     good_data = list()
 
-    with open(test_file) as csvfile:
+    with open(scanned_isbn_file) as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
         next(reader)
         for row in reader:
@@ -24,10 +27,6 @@ def test_load():
             for value in row:
                 try:
                     value = convert(value)
-                except:
-                    pass
-                try:
-                    value = int(value)
                 except:
                     pass
                 if value == '':
@@ -38,15 +37,28 @@ def test_load():
 
 
     data = DataIngest(
-        input_file=test_file,
+        scanned_isbn_file=scanned_isbn_file,
+        authors_file=authors_file,
+        output_file=output_file,
+        editions_file=editions_file,
     )
 
     data.initialize_db()
-    data.load()
-
-    assert data.connection.sql("select count(*) from input_data;").fetchone()[0] > 0
-    assert data.connection.sql("select * from input_data;").fetchall() == good_data
+    data.create_personal_library()
+    data.update_personal_library()
 
 
+    assert data.connection.sql("select count(*) from library;").fetchone()[0] > 0
+    current_library = data.connection.sql("""
+                                select entry::timestamp as entry
+                                      ,location
+                                      ,barcode
+                                      ,ownership_status
+                                      ,comment
+                                  from library;
+                               """).fetchall() 
+    assert current_library[0] == good_data[0]
+    assert len(good_data) == 27 # There are duplicates
+    assert len(current_library) == 24 # No duplicates should be there
 
 
